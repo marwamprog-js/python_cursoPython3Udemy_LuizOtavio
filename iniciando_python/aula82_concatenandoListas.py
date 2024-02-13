6b913e5008da11396374b284c977a3a635671aa9
marwamprog-js <marwamprog.js@gmail.com>
2024-02-13 09:15:35 -0300

    Estudando Concatenacao de listas refatorando

--- iniciando_python/aula82_concatenandoListas.py
+++ iniciando_python/aula82_concatenandoListas.py
@@ -1,4 +1,9 @@
 
 lista_a = [1,2,3]
 lista_b = [4,5,6]
-lista_c = lista_b
+
+# Uniao de listas
+lista_c = lista_a + lista_b
+
+# Apenas extende lista_a com lista_b, não atribui valor
+lista_d = lista_a.extend(lista_b)
